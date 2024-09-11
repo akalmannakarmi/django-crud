0465d562cc62171a76b509f4815adfa4fd740435
akal <akalnakarmi1@gmail.com>
2024-09-11 12:35:07 +0545

    added models

--- crud/models.py
+++ crud/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Address(models.Model):
+    country = models.CharField(max_length=50)
+    state = models.CharField(max_length=50)
+    city = models.CharField(max_length=50)
+
+class Info(models.Model):
+    firstName = models.CharField(max_length=25)
+    middleName = models.CharField(max_length=25)
+    lastName = models.CharField(max_length=25)
+    dob = models.DateField()
+    address = models.ForeignKey(Address,null=True,on_delete=models.SET_NULL,related_name="infos")
